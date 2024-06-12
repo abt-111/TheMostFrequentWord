7073bb28d340cc6d91c5b0341a5533352b49ae2c
abt93 <anthony.temmel@gmail.com>
2024-06-12 12:20:00 +0200

    Ajout des variable, split du paragraph et affichage des mots nettoyé

--- TheMostFrequentWord/Program.cs
+++ TheMostFrequentWord/Program.cs
@@ -2,6 +2,16 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+        string[] bannedWords = {"hit"};
+        string[] splitParagraph;
+        char[] charsToTrim = { ' ', ',', '.', '!', '?', ';' };
+
+        splitParagraph = paragraph.Split();
+
+        foreach(string word in splitParagraph)
+        {
+            Console.WriteLine(word.Trim(charsToTrim));
+        }
     }
 }
