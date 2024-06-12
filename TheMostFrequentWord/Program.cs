e42ceb97f55f0b47540a02c75a38e2f3685f02cb
abt93 <anthony.temmel@gmail.com>
2024-06-12 17:21:28 +0200

    Ajout du comptage

--- TheMostFrequentWord/Program.cs
+++ TheMostFrequentWord/Program.cs
@@ -4,25 +4,45 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+        bool isBanned = false;
+        int maxFrequency = 0, counter = 0;
+        string mostFrequentWord ="";
+        string paragraph = "Bob hit a ball, the hit BALL flew The tHE far after thE it was hit.";
         string[] bannedWords = {"hit"};
         string[] splitParagraph;
         char[] charsToTrim = { ' ', ',', '.', '!', '?', ';' };
 
         splitParagraph = paragraph.Split();
 
-        int i = 0;
-        string test = "ball";
-
-        // Comptage avec valeurs fixes pour test
+        // On parcourt chaque mot du paragraph
         foreach(string word in splitParagraph)
         {
-            if(String.Equals(test, word.Trim(charsToTrim), StringComparison.OrdinalIgnoreCase))
+            isBanned = false;
+            // On vérifie que le mot n'est pas interdit
+            if (bannedWords.Contains(word.Trim(charsToTrim)))
             {
-                i++;
+                isBanned = true;
+            }
+
+            if(!isBanned)
+            {
+                counter = 0;
+
+                foreach (string wordBis in splitParagraph)
+                {
+                    // On compare le mot à chaque mot du paragraphe en ne tenant pas compte de la "case"
+                    if (String.Equals(word.Trim(charsToTrim), wordBis.Trim(charsToTrim), StringComparison.OrdinalIgnoreCase))
+                    {
+                        counter++;
+                    }
+                }
+                if(maxFrequency < counter)
+                {
+                    mostFrequentWord = word.Trim(charsToTrim);
+                    maxFrequency = counter;
+                }
             }
         }
-
-        Console.WriteLine(i);
+        Console.WriteLine(mostFrequentWord);
     }
 }
